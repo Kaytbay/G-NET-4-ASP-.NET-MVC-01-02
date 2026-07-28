87348c4989650d873b1234d64be2610edb227a5d
Kaytbay <mohamedkaytbay@gmail.com>
2026-07-28 18:10:49 +0300

    Plan index view&controller

---
+++ GymSys/Controllers/PlansController.cs
@@ -0,0 +1,25 @@
+﻿using GymSys.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymSys.Controllers
+{
+    public class PlansController : Controller
+    {
+        private readonly GymDbContext dbContext;
+
+        public PlansController()
+        {
+            dbContext = new GymDbContext();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var Plans =await dbContext.Plans.ToListAsync();
+
+            return View(Plans);
+        }
+
+         
+    }
+}
