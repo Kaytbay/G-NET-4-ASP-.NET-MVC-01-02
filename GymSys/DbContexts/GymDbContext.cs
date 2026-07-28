e57198d33678bcbb3d8d6a218474d045cd12247f
Kaytbay <mohamedkaytbay@gmail.com>
2026-07-28 16:05:21 +0300

    Add Plan Mod,Con,Mig

---
+++ GymSys/DbContexts/GymDbContext.cs
@@ -0,0 +1,25 @@
+﻿using GymSys.Configurations;
+using GymSys.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymSys.DbContexts
+{
+    public class GymDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=GymSysDb;Trusted_Connection=true;TrustServerCertificate=true;");
+        }
+        
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Plan>(new PlanConfiguration());
+        }
+
+        public DbSet<Plan> Plans { get; set; }
+
+
+
+
+    }
+}
